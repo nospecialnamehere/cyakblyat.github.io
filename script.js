1faf926bbdd6939428f85767e2608dea14c68737
Aman-exp <Aman-exp@users.noreply.github.com>
2026-02-22 10:49:20 +0000

    deploy: nospecialnamehere/Calc@d1abe9eff6287ef4b49b7a44de033fd0408ad126

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const DEFAULTS = {
-  t: 1.59,
+  t: 1.58,
   req: 4,
   g: 72,
-  v_exp: 277,
-  r_exp: 274
+  v_exp: 317,
+  r_exp: 314
 };
 
 
@@ -101,6 +101,7 @@ window.addEventListener("DOMContentLoaded",()=>{
   createRandomStars(200);
   setInterval(()=>blastStars(3),1e4)
 });
+
 
 
 
